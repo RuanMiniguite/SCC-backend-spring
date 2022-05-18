4cb832997639c5523676f53bb35b33030db94065
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-18 20:59:03 -0300

    :tada: First code backend

---
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/SccApplication.java
@@ -0,0 +1,13 @@
+package edu.ifes.ci.si.les.scc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccApplication.class, args);
+	}
+
+}
---
+++ Code/backend/scc/src/test/java/edu/ifes/ci/si/les/scc/SccApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifes.ci.si.les.scc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
