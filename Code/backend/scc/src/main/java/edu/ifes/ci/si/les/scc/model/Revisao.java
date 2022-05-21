a5515f3942eaef11280b868a6710e6fd3ee48495
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-20 23:47:56 -0300

    :package: Validation Revisão

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/Revisao.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/Revisao.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Data
@@ -17,15 +19,21 @@ public class Revisao implements Serializable{
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer codRevisao;
-
+	
+	@NotBlank(message = "A data da Revisão deve ser preenchido")
+    @JsonFormat(pattern = "yyyy-MM-dd")
 	private Date data;
-
+	
+	@NotBlank(message = "Valor da revisão deve ser preenchido")
+    @Digits(integer=6, fraction=2, message = "Valor da revisão deve ser preenchido com dígitos")
 	private Double valor;
 	
+	@NotNull(message = "O Funcionario deve ser preenchido")
 	@ManyToOne
 	@JoinColumn(name="codFuncionario")
 	private Funcionario funcionario;
 	
+	@NotNull(message = "A Moto deve ser preenchido")
 	@ManyToOne
 	@JoinColumn(name="codMoto")
 	private Moto moto;
