9a01eb227e6f0dcffd46baf7de846dc318c681c8
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-20 23:57:13 -0300

    :package: Validation TaxaComissão

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/TaxaComissao.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/TaxaComissao.java
@@ -3,6 +3,7 @@ package edu.ifes.ci.si.les.scc.model;
 import java.io.Serializable;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 @Data
@@ -16,13 +17,21 @@ public class TaxaComissao implements Serializable{
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer codTaxaComissao;
-
+	
+	@NotBlank(message = "A taxa da comissão deve ser preenchido")
+    @Digits(integer=3, fraction=2, message = "A taxa da comissão e deve ser preenchido com dígitos")
 	private Double taxa;
-
+	
+	@NotBlank(message = "Valor limite deve ser preenchido")
+    @Digits(integer=6, fraction=2, message = "Valor limite deve ser preenchido com dígitos")
 	private Double valorLimite;
-
+	
+	@Digits(integer=1, fraction=0, message = "O Tempo de casa deve ser preenchido com um valor inteiro")
 	private Integer anoCasa;
-
+	
+	@Column(length = 25)
+	@NotBlank(message = "O cargo do funcionario deve ser preenchido")
+	@Size(min = 2, max = 25, message = "O cargo do funcionario deve ter entre 2 e 25 letras")
 	private String cargo;
 
 }
