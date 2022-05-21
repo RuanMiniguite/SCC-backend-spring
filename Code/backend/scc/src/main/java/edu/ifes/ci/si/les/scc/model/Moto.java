d46335a37d2c0a3ebbe20fab0fcdeab299cb645a
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-20 23:18:58 -0300

    :package: Validation Moto

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/Moto.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/Moto.java
@@ -3,6 +3,7 @@ package edu.ifes.ci.si.les.scc.model;
 import java.io.Serializable;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 @Data
@@ -16,33 +17,59 @@ public class Moto implements Serializable{
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer codMoto;
-
+	
+	@Column(length = 50)
+    @NotBlank(message = "O modelo deve ser preenchido")
+    @Size(min = 2, max = 50, message = "O modelo deve ter entre 2 e 50 letras")
 	private String modelo;
-
+	
+	@Column(length = 50)
+    @NotBlank(message = "A marca deve ser preenchido")
+    @Size(min = 2, max = 50, message = "A marca deve ter entre 2 e 50 letras")
 	private String marca;
-
+	
+	@Digits(integer=4, fraction=0, message = "O ano fabricação deve valor inteiro")
 	private Integer anoFabricacao;
-
+	
+	@Digits(integer=4, fraction=0, message = "O ano modelo deve valor inteiro")
 	private Integer anoModelo;
-
+	
+	@Column(length = 50)
+    @NotBlank(message = "A cor deve ser preenchido")
+    @Size(min = 2, max = 50, message = "A cor deve ter entre 2 e 50 letras")
 	private String cor;
-
+	
+	@Column(length = 50)
+    @NotBlank(message = "A cor deve ser preenchido")
+    @Size(min = 2, max = 50, message = "A cor deve ter entre 2 e 50 letras")
 	private String combustivel;
-
+	
+	@NotBlank(message = "Valor da cilindrada deve ser preenchido")
+    @Digits(integer=2, fraction=2, message = "Valor da cilindrada deve ser preenchido com um valor decimal")
 	private Double cc;
-
+	
+	@Column(length = 50)
+    @NotBlank(message = "O chassi deve ser preenchido")
+    @Size(min = 2, max = 50, message = "O chassi deve ter entre 2 e 50 letras e numeros")
 	private String chassi;
-
+	
+	@NotBlank(message = "Valor da Moto deve ser preenchido")
+    @Digits(integer=2, fraction=2, message = "Valor da Moto deve ser preenchido com um valor decimal")
 	private Double valor;
-
+	
+	@Column(length = 25)
+    @NotBlank(message = "A placa deve ser preenchido")
+	@Size(min = 6, max = 8, message = "A placa da moto deve ter 7 caracteres")
 	private String placa;
-
+	
+	@NotNull(message = "O Tipo da moto deve ser preenchido")
 	@ManyToOne
 	@JoinColumn(name="codTipo")
 	private TipoMoto tipoMoto;
 	
+	@NotNull(message = "O cliente deve ser preenchido")
 	@ManyToOne
-	@JoinColumn(name="codRecall")
+	@JoinColumn(name="codCliente")
 	private Cliente cliente;
 
 }
