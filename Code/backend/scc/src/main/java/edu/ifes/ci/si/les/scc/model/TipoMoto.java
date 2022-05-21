0f33fdbfdeb22dbbc59e53810cf27404c5b9f424
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-21 00:12:37 -0300

    :package: Validation TipoMoto

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/TipoMoto.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/TipoMoto.java
@@ -3,6 +3,7 @@ package edu.ifes.ci.si.les.scc.model;
 import java.io.Serializable;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 @Data
@@ -16,7 +17,10 @@ public class TipoMoto implements Serializable{
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer codTipo;
-
+	
+	@Column(length = 50)
+    @NotBlank(message = "O tipo da moto deve ser preenchido")
+    @Size(min = 2, max = 50, message = "Nome do tipo moto deve ter entre 2 e 50 letras")
 	private String nome;
 
 }
