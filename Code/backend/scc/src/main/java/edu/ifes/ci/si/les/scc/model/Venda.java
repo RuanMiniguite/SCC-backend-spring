2a8ddf490117e9aaefe515bfce8129d2020ad5b5
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-21 00:15:49 -0300

    :package: Validation Venda

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/Venda.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/Venda.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Data
@@ -17,19 +19,27 @@ public class Venda implements Serializable{
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer codVenda;
-
+	
+	@NotBlank(message = "Data da venda ser preenchida")
+    @JsonFormat(pattern = "yyyy-MM-dd")
 	private Date data;
-
+	
+	@NotBlank(message = "O valor da venda deve ser preenchido")
+	@Digits(integer=6, fraction=2, message = "O valor da venda deve ser preenchido com dígitos")
 	private Double valor;
 
 	private Boolean pago;
-
+	
+	@NotBlank(message = "O valor do desconto deve ser preenchido")
+	@Digits(integer=6, fraction=2, message = "O valor do desconto deve ser preenchido com dígitos")
 	private Double desconto;
 	
+	@NotNull(message = "O cliente deve ser preenchido")
 	@ManyToOne
 	@JoinColumn(name="codCliente")
 	private Cliente cliente;
 	
+	@NotNull(message = "O funcionario deve ser preenchido")
 	@ManyToOne
 	@JoinColumn(name="codFuncionario")
 	private Funcionario funcionario;
