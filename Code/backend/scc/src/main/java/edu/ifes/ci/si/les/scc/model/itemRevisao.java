4f1abe631961b08107a94e541271ac10abbaf620
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-20 22:56:56 -0300

    :package: Validation ItemRevisão

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/itemRevisao.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/itemRevisao.java
@@ -3,6 +3,10 @@ package edu.ifes.ci.si.les.scc.model;
 import java.io.Serializable;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -18,10 +22,16 @@ public class itemRevisao implements Serializable{
     @EmbeddedId
 	private itemRevisaoPK id = new itemRevisaoPK();
 	
+	@Column(length = 50)
+    @NotBlank(message = "Nome do Produto deve ser preenchido")
+    @Size(min = 2, max = 50, message = "Nome do Produto deve ter entre 2 e 50 letras")
 	private String nome;
 
+	@NotBlank(message = "Valor Unitario deve ser preenchido")
+    @Digits(integer=6, fraction=2, message = "Valor Unitario deve ser preenchido com dígitos")
 	private Double valorUnitario;
-
+	
+	@Digits(integer=1, fraction=0, message = "A quantidade do Produto deve ser preenchido com um valor inteiro")
 	private Integer qtd;
 
 }
