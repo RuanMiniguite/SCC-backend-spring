ee42d22fefb9e6915c7623cf860c9d9528a4d5c3
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-21 00:10:55 -0300

    :package: Validation Recall

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/Recall.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/model/Recall.java
@@ -3,6 +3,10 @@ package edu.ifes.ci.si.les.scc.model;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.*;
+import javax.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
 import lombok.*;
 
 @Entity
@@ -17,19 +21,35 @@ public class Recall implements Serializable{
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer codRecall;
-
+	
+	@Column(length = 50)
+	@NotBlank(message = "Nome do recall deve ser preenchido")
+	@Size(min = 2, max = 50, message = "Nome do produto deve ter entre 2 e 50 letras")
 	private String nome;
-
+	
+	@NotBlank(message = "Data de cadastro do recall ser preenchida")
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date dataCadastro;
-
+	
+	@Column(length = 50)
+	@NotBlank(message = "O chassi inicial deve ser preenchido")
+	@Size(min = 2, max = 50, message = "O chassi inicial deve ter entre 2 e 50 letras e numeros")
 	private String chassiIni;
 
+	@Column(length = 50)
+	@NotBlank(message = "O chassi final deve ser preenchido")
+	@Size(min = 2, max = 50, message = "O chassi final deve ter entre 2 e 50 letras e numeros")
 	private String chassiFim;
-
+	
+	@Digits(integer=4, fraction=0, message = "O ano do modelo deve ser preenchido com um valor inteiro")
 	private Integer anoModelo;
-
+	
+	@NotBlank(message = "Data inicial ser preenchida")
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date dataIni;
-
+	
+	@NotBlank(message = "Data final ser preenchida")
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date dataFim;
 	
 	@ManyToOne
