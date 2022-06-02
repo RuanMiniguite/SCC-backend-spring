9170bce1ac27c4436f4911b7bb970a8b80c41f68
xzanella <lhenriquezan@gmail.com>
2022-06-01 21:53:19 -0300

    :package: add services exceptions

---
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,41 @@
+package edu.ifes.ci.si.les.scc.controllers.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import edu.ifes.ci.si.les.scc.services.exceptions.BusinessRuleException;
+import edu.ifes.ci.si.les.scc.services.exceptions.ConstraintException;
+import edu.ifes.ci.si.les.scc.services.exceptions.DataIntegrityException;
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+    
+    @ExceptionHandler(ConstraintException.class)
+    public ResponseEntity<StandardError> constraint(ConstraintException e, HttpServletRequest request) {
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Restrição de dados", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+    
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity<StandardError> businessRule(BusinessRuleException e, HttpServletRequest request) {
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.CONFLICT.value(), "Regra de negócio", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(err);
+            
+    }
+}
