041efe0db6fba59dafe864bc635120f8a7c3e41f
Patricia Darós <prdaros@gmail.com>
2022-06-02 23:46:05 -0300

    :package: Add route Recall

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/controllers/RecallController.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/controllers/RecallController.java
@@ -6,6 +6,63 @@
 
 package edu.ifes.ci.si.les.scc.controllers;
 
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ifes.ci.si.les.scc.model.Recall;
+import edu.ifes.ci.si.les.scc.services.RecallService;
+import edu.ifes.ci.si.les.scc.services.exceptions.ConstraintException;
+
+@RestController
+@RequestMapping(value = "/recall")
 public class RecallController {
 
+    @Autowired
+    private RecallService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Collection<Recall>> findAll() {
+        Collection<Recall> collection = service.findAll();
+        return ResponseEntity.ok().body(collection);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Recall> find(@PathVariable Integer id) {
+    	Recall obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Recall> insert(@Valid @RequestBody Recall obj, BindingResult br) {
+        if (br.hasErrors())
+        	throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
+        obj = service.insert(obj);
+        return ResponseEntity.ok().body(obj);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Recall> update(@Valid @RequestBody Recall obj, BindingResult br) {
+        if (br.hasErrors())
+        	throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
+        obj = service.update(obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
+
--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/services/RecallService.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/services/RecallService.java
@@ -6,6 +6,61 @@
 
 package edu.ifes.ci.si.les.scc.services;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import edu.ifes.ci.si.les.scc.model.Recall;
+import edu.ifes.ci.si.les.scc.repositories.RecallRepository;
+import edu.ifes.ci.si.les.scc.services.exceptions.DataIntegrityException;
+import edu.ifes.ci.si.les.scc.services.exceptions.ObjectNotFoundException;
+
+@Service
 public class RecallService {
 
+    @Autowired
+    private RecallRepository repository;
+
+    public Recall findById(Integer id) {
+        Recall obj = repository.findById(id).get();
+        if (obj == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Recall.class.getName());
+        }
+        return obj;
+    }
+
+    public Collection<Recall> findAll() {
+        return repository.findAll();
+    }
+
+    public Recall insert(Recall obj) {
+    	obj.setCodRecall(null);
+        try {
+        	return repository.save(obj);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Campo(s) obrigatório(s) do Recall não foi(foram) preenchido(s)");
+        }
+    }
+
+    public Recall update(Recall obj) {
+    	findById(obj.getCodRecall());
+        try {
+        	return repository.save(obj);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Campo(s) obrigatório(s) do Recall não foi(foram) preenchido(s)");
+        }
+    }
+
+    public void delete(Integer id) {
+        findById(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir este Recall!");
+        }
+    }
+
 }
+
