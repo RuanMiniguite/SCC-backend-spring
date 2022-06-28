3b40b56224f524e5d292a317ad66ba2eb84e5735
RuanMiniguite <ruanminiguite@gmail.com>
2022-06-28 18:52:29 -0300

    :package: Add VendaRepository

--- Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/repositories/VendaRepository.java
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/repositories/VendaRepository.java
@@ -1,11 +1,29 @@
+//Autor
+//------------------------------
+//Ruan Pezzin Miniguite
+//20181si018
+//------------------------------
+
 package edu.ifes.ci.si.les.scc.repositories;
+
+import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Query;
 
 import edu.ifes.ci.si.les.scc.model.Venda;
 
 @Repository
 public interface VendaRepository extends JpaRepository<Venda, Integer>{
-
+	
+	@Transactional(readOnly = true)
+	@Query(value = "SELECT venda.* FROM venda WHERE venda.pago = 'false' AND venda.cod_cliente = ?1", nativeQuery = true)
+	public Collection<Venda> findByClienteDebito(Integer cod_cliente);
+	
+	@Transactional(readOnly = true)
+	@Query(value = "SELECT venda.* FROM venda WHERE venda.cod_cliente = ?1", nativeQuery = true)
+	public Collection<Venda> findByCliente(Integer cod_cliente);
+	
 }
