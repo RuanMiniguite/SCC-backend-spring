6906cae242d612c07a48aa9d00c477387cc7341f
RuanMiniguite <ruanminiguite@gmail.com>
2022-06-28 18:51:53 -0300

    :package: Add VendaController

---
+++ Code/backend/scc/src/main/java/edu/ifes/ci/si/les/scc/controllers/VendaController.java
@@ -0,0 +1,66 @@
+//Autor
+//------------------------------
+//Ruan Pezzin Miniguite
+//20181si018
+//------------------------------
+
+package edu.ifes.ci.si.les.scc.controllers;
+
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ifes.ci.si.les.scc.model.Venda;
+import edu.ifes.ci.si.les.scc.services.VendaService;
+import edu.ifes.ci.si.les.scc.services.exceptions.ConstraintException;
+
+@RestController
+@RequestMapping(value = "/vendas")
+public class VendaController {
+	
+	@Autowired
+	private VendaService service;
+	
+	 @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Collection<Venda>> findAll() {
+        Collection<Venda> collection = service.findAll();
+        return ResponseEntity.ok().body(collection);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Venda> find(@PathVariable Integer id) {
+    	Venda obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Venda> insert(@Valid @RequestBody Venda obj, BindingResult br) {
+        if (br.hasErrors())
+        	throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
+        obj = service.insert(obj);
+        return ResponseEntity.ok().body(obj);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Venda> update(@Valid @RequestBody Venda obj, BindingResult br) {
+        if (br.hasErrors())
+        	throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
+        obj = service.update(obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
